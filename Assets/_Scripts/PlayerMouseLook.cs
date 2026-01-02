6dff93788cd1cbc85cb5403c7e2888b26f67da36
nuhwork-kor <nuhwork2396@gmail.com>
2026-01-02 18:01:59 +0900

    임시저장

---
+++ Assets/_Scripts/PlayerMouseLook.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public Transform cameraRoot;
+
+    [Header("Setting")]
+    public float mouseSensitivity = 700f;
+    public float minPitch = -90f;
+    public float maxPitch = 90f;
+
+    float pitch;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Y축 좌우 회전 Player
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        //X축 상하 회전 CameraRoot
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        cameraRoot.localRotation = Quaternion.Euler(pitch, 0, 0);
+    }
+}
